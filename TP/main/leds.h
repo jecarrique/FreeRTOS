c4f8efc97a4dc8513e8d91090e499c36467d90d8
Juan Esteban Carrique <jecarrique@sinc.unl.edu.ar>
2025-05-04 08:51:59 -0300

    add prototipo de funciones en led.h

--- TP/main/leds.h
+++ TP/main/leds.h
@@ -1,4 +1,17 @@
 #ifndef LEDS_H
 #define LEDS_H
 
+void init_leds(void);
+
+void startActionRedLed(void);
+void stopActionRedLed(void);
+
+
+void startActionGreenLed(void);
+void stopActionGreenLed(void);
+
+
+// void hndlTimerLedRojo(void);
+
+
 #endif /* LEDS_H */
